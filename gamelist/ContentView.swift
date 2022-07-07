c39f9b7d64c8a2e482be6da908aa8fe4ce642f5e
Harry Hocker <harry_hocker@icloud.com>
2022-07-06 21:19:11 -0400

    Initial Commit

---
+++ gamelist/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gamelist
+//
+//  Created by Harry Hocker on 7/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ gamelist/gamelistApp.swift
@@ -0,0 +1,17 @@
+//
+//  gamelistApp.swift
+//  gamelist
+//
+//  Created by Harry Hocker on 7/6/22.
+//
+
+import SwiftUI
+
+@main
+struct gamelistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
