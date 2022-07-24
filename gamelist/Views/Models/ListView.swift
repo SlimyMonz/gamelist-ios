5f5f509e49a2d66c2a46eeec3d95a3a4aed59743
Harry Hocker <harry_hocker@icloud.com>
2022-07-24 14:04:47 -0400

    Began work on making the game list

---
+++ gamelist/Views/Models/ListView.swift
@@ -0,0 +1,29 @@
+//
+//  ListView.swift
+//  gamelist
+//
+//  Created by Harry Hocker on 7/23/22.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    
+    
+    var body: some View {
+        
+        
+        
+        
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListView()
+    }
+}
+
+
+
