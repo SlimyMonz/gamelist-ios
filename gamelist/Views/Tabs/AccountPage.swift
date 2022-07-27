4608fe0845bfcfe5cde61a56d41801e193d35519
Harry Hocker <harry_hocker@icloud.com>
2022-07-26 21:04:14 -0400

    Commit before switching.

--- gamelist/Views/Tabs/AccountPage.swift
+++ gamelist/Views/Tabs/AccountPage.swift
@@ -15,21 +15,33 @@ struct AccountPage: View {
         
         NavigationView {
             VStack {
-                Text(dvm.username)
-                Text(dvm.firstName)
-                Text(dvm.lastName)
-                Text(dvm.id)
+                
+                Text("Username:")
+                    .padding(.top, 300)
+                CTextField(dvm.username)
+                    .padding(.bottom)
+                Text("Name: ")
+                HStack {
+                    CTextField(dvm.firstName)
+                    CTextField(dvm.lastName)
+                }.padding(.bottom)
+                Text("Email: ")
+                CTextField(dvm.email)
+                    .padding(.bottom)
+                
+                Text("User ID: " + dvm.id)
+                    .offset(y: 130)
                 
                 Text("Logout")
-                    .onTapGesture{dvm.verified = false}
                     .foregroundColor(.red)
-                    .padding()
-                
+                    .padding(20)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 25)
+                            .stroke(.red, lineWidth: 2)
+                    )
+                    .padding(.top, 20)
             }
         }
-        .onAppear(perform:{
-            print("hey")
-        })
     }
 }
 
@@ -40,3 +52,17 @@ struct AccountPage_Previews: PreviewProvider {
 }
 
 
+struct CTextField: View {
+
+    let text: String
+    
+    init(_ text: String) {
+        self.text = text
+    }
+    
+    var body: some View {
+        Text(text)
+            .foregroundColor(.primary)
+            .font(.headline)
+    }
+}
